ba19ea4dd98cda691f57954d2e778d7e9ab4a5ea
Raja Gangopadhya <rgangopadhya@ucla.edu>
2017-12-31 10:19:35 -0800

    Add cover image

--- bbook_backend/api/serializers/story.py
+++ bbook_backend/api/serializers/story.py
@@ -1,6 +1,10 @@
 from dynamic_rest.serializers import (
   DynamicRelationField,
 )
+from dynamic_rest.fields import (
+  DynamicMethodField,
+)
+from rest_framework.serializers import FileField
 from .base import BaseSerializer
 from bbook_backend.models import Story
 
@@ -14,6 +18,7 @@ class StorySerializer(BaseSerializer):
     fields = (
       'creator',
       'created_at',
+      'cover_image',
       'id',
       'recordings',
     )
@@ -22,3 +27,18 @@ class StorySerializer(BaseSerializer):
     'bbook_backend.api.serializers.SceneRecordingSerializer',
     many=True,
   )
+
+  cover_image = DynamicMethodField(
+    requires=['recordings.']
+  )
+
+  def get_cover_image(self, instance):
+    image = None
+    try:
+      image = min(
+        instance.recordings.all(), key=lambda rec: rec.order
+      ).scene.image
+    except:
+      pass
+
+    return FileField().to_representation(image)
--- tests/unit/api/test_story.py
+++ tests/unit/api/test_story.py
@@ -6,6 +6,7 @@ from moto import mock_s3
 import boto3
 from bbook_backend.models import (
   Scene,
+  SceneRecording,
   Story,
 )
 
@@ -14,10 +15,22 @@ class StoryAPITestCase(TestCase):
 
   def setUp(self):
     self.client = Client()
+    self.mock_s3 = mock_s3()
+    self.mock_s3.start()
+    conn = boto3.resource('s3', region_name='us-east-1')
+    conn.create_bucket(Bucket='b-book-test')
+    self.scene_image = SimpleUploadedFile('test.jpg', b'wooo')
     self.admin = User.objects.create(username='admin')
     self.admin.set_password('blah')
     self.admin.save()
+    self.scene = Scene.objects.create(
+      creator=self.admin,
+      image=self.scene_image
+    )
     self.story = Story.objects.create(creator=self.admin)
+
+  def tearDown(self):
+    self.mock_s3.stop()
 
   def test_can_list_stories(self):
     self.client.login(username='admin', password='blah')
@@ -25,3 +38,21 @@ class StoryAPITestCase(TestCase):
     self.assertEqual(response.status_code, 200)
     data = response.json()['stories']
     self.assertEqual(len(data), 1)
+    story = data[0]
+    self.assertIsNone(story['cover_image'])
+
+  def test_get_cover_image(self):
+    # now add a recording
+    self.client.login(username='admin', password='blah')
+    recording = SimpleUploadedFile('recording.mp3', b'yep')
+    sr = SceneRecording.objects.create(
+      recording=recording,
+      story=self.story,
+      scene=self.scene,
+    )
+    response = self.client.get('/v0/stories/?include[]=recordings.')
+    self.assertEqual(response.status_code, 200)
+    data = response.json()['stories']
+    self.assertEqual(len(data), 1)
+    story = data[0]
+    self.assertIn(self.scene.image.file.name, story['cover_image'])
